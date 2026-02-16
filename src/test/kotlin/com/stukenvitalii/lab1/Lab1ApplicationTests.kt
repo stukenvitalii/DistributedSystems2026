228241f16ba10ff1b6226e4d9d7e9fdee419570e
stukenvitalii <stukenvitalii@gmail.com>
2026-02-16 22:41:47 +0300

    Initial commit

---
+++ src/test/kotlin/com/stukenvitalii/lab1/Lab1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.stukenvitalii.lab1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Lab1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
